e42a79f2fbf5eb64d6d0527269a0974569d899e6
Andrico Zacharia <andricozach@gmail.com>
2022-04-18 00:33:51 +0100

    Working version 0.2 with scraper, and step 2 summary preview

--- apps/home/tests.py
+++ apps/home/tests.py
@@ -1,8 +1,42 @@
-# -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
+import string
+import random
+import time
 
-from django.test import TestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-# Create your tests here.
+
+class FunctionalTests(StaticLiveServerTestCase):
+    # Use a predefined fixture from a test database
+    # fixtures = ['masterDatabase.json']
+
+    def testSignUp(self):
+        # Go to sign up page
+        # Initialise driver
+        self.driver = webdriver.Chrome('apps/home/chromedriver')
+
+        # direct the webdriver to where the browser file is:
+        # your secret credentials:
+        email = "andricozach@gmail.com"
+        password = ",6CWV7TKk(Rr3mJ"
+        # Go to linkedin and login
+        self.driver.get('https://www.linkedin.com/login')
+        #time.sleep(.5)
+        self.driver.find_element_by_id('username').send_keys(email)
+        #time.sleep(.5)
+        self.driver.find_element_by_id('password').send_keys(password)
+        #time.sleep(.5)
+        self.driver.find_element_by_id('password').send_keys(Keys.RETURN)
+        #time.sleep(1)
+
+
+        # Go to sign up page
+        self.driver.get('https://www.linkedin.com/in/ajrobbins/')
+        time.sleep(3)
+
+        aboutSection = self.driver.find_element_by_class_name("pv-shared-text-with-see-more").text
+        print(aboutSection)
+
+        # Finish test and quit driver
+        self.driver.quit()
--- apps/home/urls.py
+++ apps/home/urls.py
@@ -11,7 +11,8 @@ urlpatterns = [
     # The home page
     path('', views.index, name='home'),
 
-    # Matches any html file
-    re_path(r'^.*\.*', views.pages, name='pages'),
+    # Summary endpoint
+    path('getSummary', views.get_LinkedinSummary),
 
-]
+    # Matches any html file
+    re_path(r'^.*\.*', views.pages, name='pages'),]
